b7a10b7747fee7fefc2f0a9a05c01f06783868ab
CarrotNuts <kain6245@gmail.com>
2021-07-23 16:42:18 +0900

    add member repository

---
+++ src/main/java/kr/carrot/springwebmvc/servlet/repository/MemberRepository.java
@@ -0,0 +1,42 @@
+package kr.carrot.springwebmvc.servlet.repository;
+
+
+import kr.carrot.springwebmvc.servlet.vo.Member;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    private static MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
